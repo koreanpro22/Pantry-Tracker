579c888c39b75853c4dbb75e18a705a57392d26e
David Kim <dhskim22@gmail.com>
2024-09-11 00:49:49 -0400

    change quantity ternary types

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,16 +1,16 @@
 export interface PantryItem {
   name: string;
-  quantity?: string;
+  quantity: string;
 }
 
 export interface quantity {
-  quantity?: string;
+  quantity: string;
 }
 
 export interface PantryItemProps {
   item: {
     name: string;
-    quantity?: string;
+    quantity: string;
   };
   pantryItems: PantryItem[];
   index: number;

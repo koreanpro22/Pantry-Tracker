d7c50ed3dbfbf355f745be0d52dc2cfe91a63556
koreanpro22 <dhskim22@gmail.com>
2024-11-02 01:33:35 -0400

    change generate route

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,24 +1,29 @@
 import { NextResponse } from 'next/server';
 import { OpenAI } from 'openai';
 
-const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_SECRET_KEY});
+const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_SECRET_KEY });
 
-export async function POST(req: any) {
+export async function POST(req: Request) {
   try {
-    const { pantryList } = await req.body();
+    const { pantryList } = await req.json();
 
     const systemPrompt = `
-        Give me 5 recipes that can be made with the ingredients provided
-        ${pantryList}
-    `
+      Give me 5 recipes that can be made with the ingredients provided:
+      ${pantryList}
+    `;
+
     const completion = await openai.chat.completions.create({
-        messages: [{role: "system", content: systemPrompt}, {role: "user", content: "Generate recipes"}],
-        model: "gpt-3.5-turbo"
-    })
-    console.log('completion ', completion)
-    return completion
+      messages: [{ role: "system", content: systemPrompt }, { role: "user", content: "Generate recipes" }],
+      model: "gpt-3.5-turbo"
+    });
+
+    console.log('completion ', completion);
+
+    return NextResponse.json(completion);
 
   } catch (error) {
-    console.log('There was an error')
+    console.error('There was an error', error);
+
+    return NextResponse.json({ error: 'There was an error processing your request' }, { status: 500 });
   }
 }
